edf6dc7d2dded1f13582304a3a6cb7b33642e181
MajorDroolz <maximhutz@gmail.com>
2023-11-03 20:02:55 -0400

    fix: small screen size accommadation

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 function LoginPage() {
   return (
     <span id="bodyWrapper">
-      <div id="loadingdiv" className="textc">
+      <form id="loadingdiv" className="textc">
         <input placeholder="username" className="uinp" />
-        <div>
-          <input placeholder="password" type="password" className="uinp margin10" />
-        </div>
-        <div>
-          <button className="loginbtn margin10">login</button>
-        </div>
+        <input placeholder="password" type="password" className="uinp margin10" />
+        <button className="loginbtn margin10">login</button>
         <div className="mv5">
           <h2>Don't have an account?</h2>
-          <button className="signupbtn margin10">sign up</button>
+          <Link to="/register" className="signupbtn margin10">sign up</Link>
         </div>
-      </div>
+      </form>
     </span>
   );
 }
